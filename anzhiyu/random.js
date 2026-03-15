dccaaa28a7afbe42d0dbbcd224bbeb49d3a040f0
Anthon985 <2032640482@qq.com>
2026-03-15 15:14:02 +0800

    Site updated: 2026-03-15 15:14:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/12/hello-world/","2026/03/13/个人博客部署-静态网站-Hexo-Github-pages/"];function toRandomPost(){
+var posts=["2026/03/13/个人博客部署-静态网站-Hexo-Github-pages/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
